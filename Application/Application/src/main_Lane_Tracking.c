f41481e7318da63753ed213f54858a1e7d772788
Ahmed Ibrahim Almorsy <eng.ahmedibrahim21@gmail.com>
2023-11-13 02:41:35 +0200

    PDF and Autonomous RC

--- Application/Application/src/main_Lane_Tracking.c
+++ Application/Application/src/main_Lane_Tracking.c
@@ -1,86 +1,86 @@
-/**********************************************************/
-/*********************** includes section *****************/
-/**********************************************************/
-#include "../include/STD_Types.h"
-#include "../include/BIT_MATH.h"
-
-
-#include "../include/DCM_Interface.h"
-
-#include "../include/GPT_Interface.h"
-
-#include "../include/Ultrasonic_Interface.h"
-
-#include "../include/SERVO_Interface.h"
-
-#include "../include/LINE_FOLLOWER_INTERFACE.h"
-
-#include "../include/App.h"
-
-/**********************************************************/
-/************** lane tracking system project **************/
-/**********************************************************/
-
-
-
-int main(){
-
-
-	/*		 Peripheral Enable Clock 			*/
-	Peripheral_EnableClock();
-
-	/*		DC motors initialization 			*/
-	DCM_Init(Configuration);
-
-
-	/*		 Timer 5 is used for delay 			*/
-	TIMER_Init(&Timer5_configuration);
-	/*		 line follower sensors initialization 			*/
-	line_follower_init();
-
-
-	while(1){
-		/*	line_follower_flag_right -->this flag is set when right sensor detects lane
-		 *  line_follower_flag_left  -->this flag is set when left sensor detects lane
-		 */
-
-		if(line_follower_flag_right==0 && line_follower_flag_left==1){
-			/*
-			 * when left sensor detects lane then car moves in opposite direction
-			 */
-			App_CarMoveRight(40);
-			line_follower_flag_left=0;
-			TIMER_DelayMS(TIMER5_SELECTION,400);
-			LEFT_LED_INT_ENABLE();
-
-		}
-		else if(line_follower_flag_right==1 && line_follower_flag_left==0){
-			/*
-			 * when right sensor detects lane then car moves in opposite direction
-			 */
-			App_CarMoveLeft(40);
-			line_follower_flag_right=0;
-			TIMER_DelayMS(TIMER5_SELECTION,400);
-			RIGHT_LED_INT_ENABLE();
-		}
-		else if(line_follower_flag_right==0 && line_follower_flag_left==0){
-			/*
-			 * no detection for lane
-			 */
-			App_CarMoveForward(20);
-		}
-		else{
-			/* If the 2-lanes are detected, it means that, The RC is out the 2-lanes*/
-			App_CarStop();
-		}
-
-
-
-
-	}
-
-
-	return 0;
-}
-
-
+///**********************************************************/
+///*********************** includes section *****************/
+///**********************************************************/
+//#include "../include/STD_Types.h"
+//#include "../include/BIT_MATH.h"
+//
+//
+//#include "../include/DCM_Interface.h"
+//
+//#include "../include/GPT_Interface.h"
+//
+//#include "../include/Ultrasonic_Interface.h"
+//
+//#include "../include/SERVO_Interface.h"
+//
+//#include "../include/LINE_FOLLOWER_INTERFACE.h"
+//
+//#include "../include/App.h"
+//
+///**********************************************************/
+///************** lane tracking system project **************/
+///**********************************************************/
+//
+//
+//
+//int main(){
+//
+//
+//	/*		 Peripheral Enable Clock 			*/
+//	Peripheral_EnableClock();
+//
+//	/*		DC motors initialization 			*/
+//	DCM_Init(Configuration);
+//
+//
+//	/*		 Timer 5 is used for delay 			*/
+//	TIMER_Init(&Timer5_configuration);
+//	/*		 line follower sensors initialization 			*/
+//	line_follower_init();
+//
+//
+//	while(1){
+//		/*	line_follower_flag_right -->this flag is set when right sensor detects lane
+//		 *  line_follower_flag_left  -->this flag is set when left sensor detects lane
+//		 */
+//
+//		if(line_follower_flag_right==0 && line_follower_flag_left==1){
+//			/*
+//			 * when left sensor detects lane then car moves in opposite direction
+//			 */
+//			App_CarMoveRight(40);
+//			line_follower_flag_left=0;
+//			TIMER_DelayMS(TIMER5_SELECTION,400);
+//			LEFT_LED_INT_ENABLE();
+//
+//		}
+//		else if(line_follower_flag_right==1 && line_follower_flag_left==0){
+//			/*
+//			 * when right sensor detects lane then car moves in opposite direction
+//			 */
+//			App_CarMoveLeft(40);
+//			line_follower_flag_right=0;
+//			TIMER_DelayMS(TIMER5_SELECTION,400);
+//			RIGHT_LED_INT_ENABLE();
+//		}
+//		else if(line_follower_flag_right==0 && line_follower_flag_left==0){
+//			/*
+//			 * no detection for lane
+//			 */
+//			App_CarMoveForward(20);
+//		}
+//		else{
+//			/* If the 2-lanes are detected, it means that, The RC is out the 2-lanes*/
+//			App_CarStop();
+//		}
+//
+//
+//
+//
+//	}
+//
+//
+//	return 0;
+//}
+//
+//
--- Application/Application/src/main_simple_autonomous_RC.c
+++ Application/Application/src/main_simple_autonomous_RC.c
@@ -1,110 +1,110 @@
-///*
-// * main_auto.c
-// *
-// *  Created on: Nov 9, 2023
-// *      Author: Misra
-// */
-//
-//
-//
-//#include "../include/STD_Types.h"
-//#include "../include/BIT_MATH.h"
-//
-//
-//#include "../include/DCM_Interface.h"
-//
-//#include "../include/GPT_Interface.h"
-//
-//#include "../include/Ultrasonic_Interface.h"
-//
-//#include "../include/SERVO_Interface.h"
-//
-//#include "../include/App.h"
-//
-///**********************************************************/
-///*********************** Global Variables *****************/
-///**********************************************************/
-//u32 DistanceForward = 0;
-//u32 DistanceRight = 0;
-//u32 DistanceLeft = 0;
-//
-//int main()
-//{
-//
-//
-//	/* Peripheral Enable Clock */
-//	Peripheral_EnableClock();
-//
-//	DCM_Init(Configuration);
-//
-//	Servo_Init();
-//
-//	UltraSonic_Init();
-//
-//	TIMER_Init(&Timer5_configuration);
-//
-//
-//
-//
-//	SERVO_SET_DIRECTION(SERVO_DIRECTION_FORWARD);
-//	TIMER_DelayMS(TIMER5_SELECTION,500);
-//	while (1)
-//	{
-//		UltraSonic_Send_Pulse();
-//		DistanceForward=UltraSonic_Measure_Distance();
-//		while(DistanceForward < 70)
-//		{
-//			App_CarStop();
-//
-//			UltraSonic_Send_Pulse();
-//			DistanceForward=UltraSonic_Measure_Distance();
-//
-//
-//			SERVO_SET_DIRECTION(SERVO_DIRECTION_RIGHT);
-//			TIMER_DelayMS(TIMER5_SELECTION,1000);
-//
-//			UltraSonic_Send_Pulse();
-//			DistanceRight=UltraSonic_Measure_Distance();
-//
-//			SERVO_SET_DIRECTION(SERVO_DIRECTION_LEFT);
-//			TIMER_DelayMS(TIMER5_SELECTION,1000);
-//
-//			UltraSonic_Send_Pulse();
-//			DistanceLeft=UltraSonic_Measure_Distance();
-//
-//			SERVO_SET_DIRECTION(SERVO_DIRECTION_FORWARD);
-//			TIMER_DelayMS(TIMER5_SELECTION,1000);
-//
-//			UltraSonic_Send_Pulse();
-//			DistanceForward=UltraSonic_Measure_Distance();
-//
-//			if(DistanceForward >70){
-//				break;
-//			}
-//			else if(DistanceRight>DistanceLeft && DistanceRight>70)
-//			{
-//				App_CarMoveRight(60);
-//				TIMER_DelayMS(TIMER5_SELECTION,400);/////////////////////////
-//			}
-//			else if(DistanceLeft>DistanceRight && DistanceLeft>70)
-//			{
-//				App_CarMoveLeft(60);
-//				TIMER_DelayMS(TIMER5_SELECTION,400);/////////////////////////
-//			}
-//
-//		}
-//
-//
-//		UltraSonic_Send_Pulse();
-//		DistanceForward=UltraSonic_Measure_Distance();
-//		while(DistanceForward > 70){
-//			UltraSonic_Send_Pulse();
-//			DistanceForward=UltraSonic_Measure_Distance();
-//			App_CarMoveForward(25);
-//		}
-//
-//
-//
-//	}
-//	return 0;
-//}
+/*
+ * main_auto.c
+ *
+ *  Created on: Nov 9, 2023
+ *      Author: Misra
+ */
+
+
+
+#include "../include/STD_Types.h"
+#include "../include/BIT_MATH.h"
+
+
+#include "../include/DCM_Interface.h"
+
+#include "../include/GPT_Interface.h"
+
+#include "../include/Ultrasonic_Interface.h"
+
+#include "../include/SERVO_Interface.h"
+
+#include "../include/App.h"
+
+/**********************************************************/
+/*********************** Global Variables *****************/
+/**********************************************************/
+u32 DistanceForward = 0;
+u32 DistanceRight = 0;
+u32 DistanceLeft = 0;
+
+int main()
+{
+
+
+	/* Peripheral Enable Clock */
+	Peripheral_EnableClock();
+
+	DCM_Init(Configuration);
+
+	Servo_Init();
+
+	UltraSonic_Init();
+
+	TIMER_Init(&Timer5_configuration);
+
+
+
+
+	SERVO_SET_DIRECTION(SERVO_DIRECTION_FORWARD);
+	TIMER_DelayMS(TIMER5_SELECTION,500);
+	while (1)
+	{
+		UltraSonic_Send_Pulse();
+		DistanceForward=UltraSonic_Measure_Distance();
+		while(DistanceForward < 70)
+		{
+			App_CarStop();
+
+			UltraSonic_Send_Pulse();
+			DistanceForward=UltraSonic_Measure_Distance();
+
+
+			SERVO_SET_DIRECTION(SERVO_DIRECTION_RIGHT);
+			TIMER_DelayMS(TIMER5_SELECTION,1000);
+
+			UltraSonic_Send_Pulse();
+			DistanceRight=UltraSonic_Measure_Distance();
+
+			SERVO_SET_DIRECTION(SERVO_DIRECTION_LEFT);
+			TIMER_DelayMS(TIMER5_SELECTION,1000);
+
+			UltraSonic_Send_Pulse();
+			DistanceLeft=UltraSonic_Measure_Distance();
+
+			SERVO_SET_DIRECTION(SERVO_DIRECTION_FORWARD);
+			TIMER_DelayMS(TIMER5_SELECTION,1000);
+
+			UltraSonic_Send_Pulse();
+			DistanceForward=UltraSonic_Measure_Distance();
+
+			if(DistanceForward >70){
+				break;
+			}
+			else if(DistanceRight>DistanceLeft && DistanceRight>70)
+			{
+				App_CarMoveRight(60);
+				TIMER_DelayMS(TIMER5_SELECTION,400);/////////////////////////
+			}
+			else if(DistanceLeft>DistanceRight && DistanceLeft>70)
+			{
+				App_CarMoveLeft(60);
+				TIMER_DelayMS(TIMER5_SELECTION,400);/////////////////////////
+			}
+
+		}
+
+
+		UltraSonic_Send_Pulse();
+		DistanceForward=UltraSonic_Measure_Distance();
+		while(DistanceForward > 70){
+			UltraSonic_Send_Pulse();
+			DistanceForward=UltraSonic_Measure_Distance();
+			App_CarMoveForward(25);
+		}
+
+
+
+	}
+	return 0;
+}
